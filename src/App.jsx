99582f1df9c21577cbc28af18259862b676af527
Asif359 <asifahammednishst@gmail.com>
2023-10-18 02:39:59 +0600

    edit on App jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Outlet,NavLink } from 'react-router-dom';
 import NavBar from './Components/Navbar/NavBar';
-import Slider from './Components/Slider/Slider';
 function App() {
   const [count, setCount] = useState(0)
   return (
