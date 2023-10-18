66789ec87bdca9b4f74f279de594246a06f1648d
Asif359 <asifahammednishst@gmail.com>
2023-10-18 09:57:26 +0600

    Added firebase config file

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -9,7 +9,7 @@ const Slider = () => {
             <div>
                 <CCarousel controls indicators >
                     <CCarouselItem>
-                        <CImage className="d-block w-100 h-[90vh] " src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 1" />
+                        <CImage className="d-block w-100 h-72 md:h-[70vh] lg:h-[90vh] " src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 1" />
                         <CCarouselCaption className="hidden d-md-block">
                             <div className='bg-gray-400 bg-opacity-30 p-2 rounded-md shadow-inherit'>
                                 <h5>First slide label</h5>
@@ -18,7 +18,7 @@ const Slider = () => {
                         </CCarouselCaption>
                     </CCarouselItem>
                     <CCarouselItem>
-                        <CImage className="d-block w-100 h-[90vh]" src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 2" />
+                        <CImage className="d-block w-100 h-72 md:h-[70vh] lg:h-[90vh]" src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 2" />
                         <CCarouselCaption className="hidden d-md-block">
                             <div className='bg-gray-400 bg-opacity-30 p-2 rounded-md shadow-inherit '>
                                 <h5>Second slide label</h5>
@@ -27,7 +27,7 @@ const Slider = () => {
                         </CCarouselCaption>
                     </CCarouselItem>
                     <CCarouselItem>
-                        <CImage className="d-block w-100 h-[90vh]" src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 3" />
+                        <CImage className="d-block w-100 h-72 md:h-[70vh] lg:h-[90vh]" src={`https://www.topgear.com/sites/default/files/2022/07/13.jpg`} alt="slide 3" />
                         <CCarouselCaption className="hidden d-md-block md:block">
                             <div className='bg-gray-400 bg-opacity-30 p-2 rounded-md shadow-inherit '>
                                 <h5>Third slide label</h5>
