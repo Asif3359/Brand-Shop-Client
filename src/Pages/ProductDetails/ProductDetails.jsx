1f38cb6d1b592b8451b1adc60276159539a1a888
Asif359 <asifahammednishst@gmail.com>
2023-10-20 18:32:31 +0600

    some edit on product details page

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -49,11 +49,11 @@ const ProductDetails = () => {
     return (
         <div className=' py-5 container mx-auto '>
 
-            <div className="relative grid grid-cols-4 flex-wrap items-center  border border-gray-200 rounded-lg shadow md:flex-row md:max-full  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <div className='flex-1 col-span-3'>
-                    <img className="  object-cover w-full rounded-t-lg h-fit md:h-full md:w-full  md:rounded-none md:rounded-l-lg " src={BrandDetails.image} alt="" />
+            <div className="relative grid grid-cols-1 lg:grid-cols-4 gap-4  items-center  border border-gray-200 ">
+                <div className='lg:col-span-3 '>
+                    <img className=" h-[50vh] md:h-[80vh] w-full  " src={BrandDetails.image} alt="" />
                 </div>
-                <div className="flex-1 grid grid-cols-1 justify-between p-4 leading-normal w-full">
+                <div className=" p-2">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight  dark:text-white">{BrandDetails.name}</h5>
                     <h5 className="mb-2 text-xl font-bold tracking-tight  dark:text-white">{BrandDetails.brandName}</h5>
                     <h5 className="mb-2  font-bold tracking-tight  dark:text-white">{BrandDetails.type}</h5>
